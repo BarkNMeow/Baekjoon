3adea946afc9207a0b87e137212c53d8c9933b2e
Lee Junmo <leejunmo84@gmail.com>
2024-07-24 18:11:02 +0900

    Solve 15681

--- _template.cpp
+++ _template.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 // Dummy variable to prevent misinterpretation
 bool __dummy__ = ios::sync_with_stdio(false);
+auto __dummy2__ = cin.tie(NULL);
+auto __dummy3__ = cout.tie(NULL);
 
 int main() {}
