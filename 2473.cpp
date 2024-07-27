e87399ce1199c972b39fc94d4a6196c2064d59ad
Lee Junmo <leejunmo84@gmail.com>
2024-07-28 04:54:35 +0900

    Optimize 2473

--- 2473.cpp
+++ 2473.cpp
@@ -15,33 +15,25 @@ int main() {
     sort(arr, arr + n);
 
     long long int min_diff = 0x7FFFFFFFFFFFFFFF;
-    int liq_a = -1, liq_b = -1, liq_other = -1;
+    int ans[3] = {0};
 
-    for (int i = 0; i < n; i++) {
-        int s = (i == 0 ? 1 : 0);
-        int e = (i == n - 1 ? n - 2 : n - 1);
+    for (int s = 0; s < n - 2; s++) {
+        int m = s + 1, e = n - 1;
 
-        while (s < e) {
-            long long int diff = arr[i] + arr[s] + arr[e];
+        while (m < e) {
+            long long int diff = arr[s] + arr[m] + arr[e];
             if (abs(diff) < min_diff) {
                 min_diff = abs(diff);
-                liq_a = arr[s], liq_b = arr[e], liq_other = arr[i];
+                ans[0] = arr[s], ans[1] = arr[m], ans[2] = arr[e];
             }
 
             if (diff > 0) {
-                do {
-                    e--;
-                } while (e == i);
+                e--;
             } else {
-                do {
-                    s++;
-                } while (s == i);
+                m++;
             }
         }
     }
-
-    int ans[3] = {liq_a, liq_b, liq_other};
-    sort(ans, ans + 3);
 
     for (auto num : ans) cout << num << ' ';
 }
