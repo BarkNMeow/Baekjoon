8654760198a9018516d149b2f5554c41ff6410d6
Lee Junmo <leejunmo84@gmail.com>
2024-08-07 01:20:43 +0900

    Solve 2258

---
+++ 2258.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Dummy variable to prevent misinterpretation
+bool __dummy__ = ios::sync_with_stdio(false);
+auto __dummy2__ = cin.tie(NULL);
+auto __dummy3__ = cout.tie(NULL);
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
+}
