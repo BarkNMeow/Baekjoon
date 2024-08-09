4d31d1ad4fe7cab4bdb309d853db01299e5f40f4
Lee Junmo <leejunmo84@gmail.com>
2024-08-09 15:07:57 +0900

    Remove redundant code

--- 12781.cpp
+++ 12781.cpp
@@ -6,10 +6,10 @@ bool __dummy__ = ios::sync_with_stdio(false);
 auto __dummy2__ = cin.tie(NULL);
 auto __dummy3__ = cout.tie(NULL);
 
-static long long x1, y1, x2, y2;
-static long long x3, y3, x4, y4;
+static int x1, y1, x2, y2;
+static int x3, y3, x4, y4;
 
-bool denom_test(long long denom, long long numer) {
+bool denom_test(int denom, int numer) {
     if (denom > 0 && (numer <= 0 || numer >= denom))
         return false;
     else if (denom < 0 && (numer >= 0 || numer <= denom))
@@ -22,12 +22,12 @@ int main() {
     cin >> x1 >> y1 >> x2 >> y2;
     cin >> x3 >> y3 >> x4 >> y4;
 
-    long long Ax = x2 - x1, Ay = y2 - y1;
-    long long Bx = x3 - x4, By = y3 - y4;
-    long long Cx = x1 - x3, Cy = y1 - y3;
+    int Ax = x2 - x1, Ay = y2 - y1;
+    int Bx = x3 - x4, By = y3 - y4;
+    int Cx = x1 - x3, Cy = y1 - y3;
 
-    long long denom = Ay * Bx - Ax * By;
-    long long A_numer = By * Cx - Bx * Cy;
+    int denom = Ay * Bx - Ax * By;
+    int A_numer = By * Cx - Bx * Cy;
 
     if (denom == 0) {
         cout << 0;
@@ -38,9 +38,8 @@ int main() {
 
     if (!intersect) {
         cout << 0;
-        return 0;
     } else {
-        long long B_numer = Ax * Cy - Ay * Cx;
+        int B_numer = Ax * Cy - Ay * Cx;
         intersect = denom_test(denom, B_numer);
         cout << (intersect ? 1 : 0);
     }
